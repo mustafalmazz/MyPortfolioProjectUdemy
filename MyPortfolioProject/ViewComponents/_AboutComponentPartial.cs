5a2fb46a51414d01fa941e070ff9f9c4f528cbcb
mustafalmazz <mustafagfb19077091@gmail.com>
2025-05-12 01:10:21 +0300

    Proje Componentlere ayırıldı

---
+++ MyPortfolioProject/ViewComponents/_AboutComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace MyPortfolioProject.ViewComponents
+{
+    public class _AboutComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
