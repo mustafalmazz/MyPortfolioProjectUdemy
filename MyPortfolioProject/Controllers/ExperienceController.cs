a575e7f714207101a154318fb34977f316254e43
mustafalmazz <mustafagfb19077091@gmail.com>
2025-05-18 19:39:57 +0300

    İstatistikler ve Mesajlar
    İstatistikler ve mesajlar için sayfa tasarımlaır yapıldı ve sayfalar dinamik hale getirildi.

--- MyPortfolioProject/Controllers/ExperienceController.cs
+++ MyPortfolioProject/Controllers/ExperienceController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MyPortfolioUdemy.DAL.Context;
+using MyPortfolioUdemy.DAL.Entities;
+using System.Security.Cryptography.X509Certificates;
 
 namespace MyPortfolioProject.Controllers
 {
@@ -10,6 +12,39 @@ namespace MyPortfolioProject.Controllers
         {
             var values = context.Experiences.ToList();
             return View(values);
+        }
+
+        public IActionResult CreateExperience()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateExperience(Experience experience)
+        {
+            context.Experiences.Add(experience);
+            context.SaveChanges();
+            return RedirectToAction(nameof(ExperienceList));
+        }
+        public IActionResult DeleteExperience(int id)
+        {
+            var value = context.Experiences.Find(id);
+            context.Experiences.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+
+        public IActionResult UpdateExperience(int id)
+        {
+            var value = context.Experiences.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult UpdateExperience(Experience experience)
+        {
+            context.Experiences.Update(experience);
+            context.SaveChanges();
+            return RedirectToAction();
         }
     }
 }
